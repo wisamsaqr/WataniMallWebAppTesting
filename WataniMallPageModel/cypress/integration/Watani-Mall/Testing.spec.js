e06492bb209712d7a49b362bb2594953d24e5f08
Wisam Saqr <wisam.saqr@gmail.com>
2022-03-18 20:04:41 +0200

    Update Testing.spec.js

--- WataniMallPageModel/cypress/integration/Watani-Mall/Testing.spec.js
+++ WataniMallPageModel/cypress/integration/Watani-Mall/Testing.spec.js
@@ -3,132 +3,132 @@
 
 describe("Watani Mall Testing", ()=>
 {
-    // before(()=>
-    // {
-    //     // Preserve cookie in every test
-    //     Cypress.Cookies.defaults({preserve: (cookie) => { return true; }})
-    // })
+     before(()=>
+     {
+         // Preserve cookie in every test
+         Cypress.Cookies.defaults({preserve: (cookie) => { return true; }})
+     })
 
 
-    // it("Verifying visiting Watani Mall homepage", ()=>
-    // {
-        // Visit the web site
-        // cy.visit("/")
-        // cy.url().should('eq', 'https://watanimall.com/')
+     it("Verifying visiting Watani Mall homepage", ()=>
+     {
+         Visit the web site
+         cy.visit("/")
+         cy.url().should('eq', 'https://watanimall.com/')
         
-        // Assert that home nav element is selected
-        // cy.get('nav#nav ul[id^="menu"] > li[id^="menu-item"]:first-child')
-        // cy.get('nav#nav ul[id^="menu"] > li[id^="menu-item"]').contains('li', 'الرئيسية')
-        //     .should('have.class', 'current-menu-item')
-    // })
+         Assert that home nav element is selected
+         cy.get('nav#nav ul[id^="menu"] > li[id^="menu-item"]:first-child')
+         cy.get('nav#nav ul[id^="menu"] > li[id^="menu-item"]').contains('li', 'الرئيسية')
+             .should('have.class', 'current-menu-item')
+     })
     
 
-    // it("Verifying navigating to all categories navigating to monitors", ()=>
-    // {
-        // navigating to all categories
-        // cy.get('nav#nav ul[id^=menu]>li[id^=menu-item] > a').contains('جميع الفئات').click()
+     it("Verifying navigating to all categories navigating to monitors", ()=>
+     {
+         navigating to all categories
+         cy.get('nav#nav ul[id^=menu]>li[id^=menu-item] > a').contains('جميع الفئات').click()
         
-        // cy.get('nav#nav ul[id^=menu] > li[id^=menu-item]:nth-child(5) > a').click()
-        // cy.url().should('include', '/all-categories')
+         cy.get('nav#nav ul[id^=menu] > li[id^=menu-item]:nth-child(5) > a').click()
+         cy.url().should('include', '/all-categories')
 
-        // Assert that heading has the text "جميع الفئات"
-        // cy.get('main#main h1.section-title').should('have.text', 'جميع الفئات')
+         Assert that heading has the text "جميع الفئات"
+         cy.get('main#main h1.section-title').should('have.text', 'جميع الفئات')
 
-        // Assert that "All Categories" nav element is selected
-        // cy.get('nav#nav ul[id^="menu"] > li[id^="menu-item"]').contains('li', 'جميع الفئات')
-        //     .should('have.class', 'current-menu-item')
-        // cy.get('nav#nav ul[id^="menu"]').find('li[id^="menu-item"]').contains('li', 'جميع الفئات')
-        //     .should('have.class', 'current-menu-item')
-    // })
+         Assert that "All Categories" nav element is selected
+         cy.get('nav#nav ul[id^="menu"] > li[id^="menu-item"]').contains('li', 'جميع الفئات')
+             .should('have.class', 'current-menu-item')
+         cy.get('nav#nav ul[id^="menu"]').find('li[id^="menu-item"]').contains('li', 'جميع الفئات')
+             .should('have.class', 'current-menu-item')
+     })
     
     context('navigating to monitors'
         +' then selecting asus filter'
         +' and sorting the results by price ascendingly',
     ()=>
     {
-        // it('Verify navigating to monitors', ()=>
-        // {
-            // Navigating to monitors
-            // cy.get('main#main div.category-row > div:nth-child(3) > a.category-item').click()
+         it('Verify navigating to monitors', ()=>
+         {
+             Navigating to monitors
+             cy.get('main#main div.category-row > div:nth-child(3) > a.category-item').click()
             
-            // cy.url().should('include', '/monitors')
+             cy.url().should('include', '/monitors')
 
-            // Assert that breadcrumb ends with text "MONITORS"
-            // cy.get('main#main > nav.woocommerce-breadcrumb').should('include.text', ' » MONITORS')
+             Assert that breadcrumb ends with text "MONITORS"
+             cy.get('main#main > nav.woocommerce-breadcrumb').should('include.text', ' » MONITORS')
 
-            // Assert that heading has the text "MONITORS"
-            // cy.get('main#main h1.page-title').should('have.text', 'MONITORS')
-        // })
+             Assert that heading has the text "MONITORS"
+             cy.get('main#main h1.page-title').should('have.text', 'MONITORS')
+         })
 
-        // it("Verifying selecting asus filter", ()=>
-        // {
-            // selecting asus filter
-            // cy.get('main#main div[data-name="manufacturer"] > div[data-value="asus"]').click()
-            // cy.get('div[data-value="asus"]').click()
-            // cy.url().should('include', '/monitors?_manufacturer=asus')
-        // })
+         it("Verifying selecting asus filter", ()=>
+         {
+             selecting asus filter
+             cy.get('main#main div[data-name="manufacturer"] > div[data-value="asus"]').click()
+             cy.get('div[data-value="asus"]').click()
+             cy.url().should('include', '/monitors?_manufacturer=asus')
+         })
 
         it("Verifying sorting the results by price ascendingly", ()=>
         {
-            // Sorting the results by price ascendingly
-            // cy.get('div.shop-page-container > div.sort-filter-row select[name="orderby"]')
-            //     .select('ترتيب حسب: الأدنى سعراً للأعلى', { force: true })
+             Sorting the results by price ascendingly
+             cy.get('div.shop-page-container > div.sort-filter-row select[name="orderby"]')
+                 .select('ترتيب حسب: الأدنى سعراً للأعلى', { force: true })
             
-            // cy.get('select[name="orderby"]')
-            // cy.url().should('include', '/monitors?orderby=price&_manufacturer=asus')
+             cy.get('select[name="orderby"]')
+             cy.url().should('include', '/monitors?orderby=price&_manufacturer=asus')
 
 
-            // cy.wait(3000)
-            // Asserting that the monitors are sorted
-            // cy.get('div.shop-products-holder div.product-col:first-child div.product-price')
-            // .then(elem=>
-            // {
-            //     if (elem.children().length > 1)
-            //     {
-            //         cy.wrap(parseFloat(elem.find('ins > span > bdi').text().slice(1).replace(",", ""))).as('previousPrice')
-            //     }
-            //     else
-            //     {
-            //         cy.wrap(parseFloat(elem.find('span > bdi').text().slice(1).replace(",", ""))).as('previousPrice')
-            //     }
+             cy.wait(3000)
+             Asserting that the monitors are sorted
+             cy.get('div.shop-products-holder div.product-col:first-child div.product-price')
+             .then(elem=>
+             {
+                 if (elem.children().length > 1)
+                 {
+                     cy.wrap(parseFloat(elem.find('ins > span > bdi').text().slice(1).replace(",", ""))).as('previousPrice')
+                 }
+                 else
+                 {
+                     cy.wrap(parseFloat(elem.find('span > bdi').text().slice(1).replace(",", ""))).as('previousPrice')
+                 }
 
-            //     // cy.log(cy.get('@previousPrice'))
-            // })
+                 // cy.log(cy.get('@previousPrice'))
+             })
 
-            // cy.get('div.shop-products-holder div.product-price')
-            // .each(elem =>
-            // {
-            //     let price
-            //     if (elem.children().length > 1)
-            //     {
-            //         price = parseFloat(elem.find('ins > span > bdi').text().slice(1).replace(",", ""))
-            //     }
-            //     else
-            //     {
-            //         price = parseFloat(elem.find('span > bdi').text().slice(1).replace(",", ""))
-            //     }
+             cy.get('div.shop-products-holder div.product-price')
+             .each(elem =>
+             {
+                 let price
+                 if (elem.children().length > 1)
+                 {
+                     price = parseFloat(elem.find('ins > span > bdi').text().slice(1).replace(",", ""))
+                 }
+                 else
+                 {
+                     price = parseFloat(elem.find('span > bdi').text().slice(1).replace(",", ""))
+                 }
 
-            //     // cy.log(price)
-            //     cy.get('@previousPrice').should('be.lte', price)
+                 // cy.log(price)
+                 cy.get('@previousPrice').should('be.lte', price)
 
-            //     cy.wrap(price).as('previousPrice')
-            // })
+                 cy.wrap(price).as('previousPrice')
+             })
         })
     })
 
     context("Adding two products to the cart then removing the first product", () =>
     {
         // go to categories page
-        it("Adding first product to the cart inside monitors page", () =>
+        it(" first product to the cart inside monitors page", () =>
         {
             // saving product price before adding it to cart
             cy.get('div.shop-products-holder div.product-col:first-child > div.product-item bdi')
                 .invoke('text').as('price')
-            // cy.get('div.shop-products-holder div.product-col:first-child > div.product-item bdi')
-            //     .invoke('text').then(t=>
-            //     {
-            //         cy.log(t)
-            //     })
+             cy.get('div.shop-products-holder div.product-col:first-child > div.product-item bdi')
+                 .invoke('text').then(t=>
+                 {
+                     cy.log(t)
+                 })
 
             
             // saving product name before adding it to cart
